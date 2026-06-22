421d0c4fb5ae5840bcb402acbf5bdad73d95eb2a
miya52099 <fachai168999@gmail.com>
2026-06-22 15:25:25 +0800

    Add files via upload

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,15 +1,6 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias["react"] = path.resolve(__dirname, "node_modules/react");
-    config.resolve.alias["react-dom"] = path.resolve(__dirname, "node_modules/react-dom");
-    config.resolve.alias["react/jsx-runtime"] = path.resolve(__dirname, "node_modules/react/jsx-runtime");
-    config.resolve.alias["react/jsx-dev-runtime"] = path.resolve(__dirname, "node_modules/react/jsx-dev-runtime");
-    config.resolve.alias["react-dom/server"] = path.resolve(__dirname, "node_modules/react-dom/server");
-    return config;
-  },
+  // Simple Next config without manual override of React aliases
 };
 
 module.exports = nextConfig;
